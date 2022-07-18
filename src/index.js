a4afc074d06973a9e0a9fd302d851ef27c151798
alfianlensun <alfianricky46@gmail.com>
2022-07-18 20:52:05 +0800

    fix merge

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
+const baseUrl = 'http://localhost:9999'
 const fastify = require('fastify')({
     logger: false
 })
+
 const mysql = require('mysql2/promise')
 const googleTTS = require('google-tts-api');
 const moment = require('moment');
+const fs = require('fs');
+const axios = require('axios');
+const path = require('path')
+fastify.register(require('@fastify/static'), {
+    root: path.join(__dirname,'..', 'audio'),
+    prefix: '/audio/',
+})
+
 
 fastify.get('/jadwal/stream', async function (request, reply) {
+      
     const connection = await mysql.createConnection({
-        host: '192.168.1.3',
+        host: '127.0.0.1',
         user: 'root',
         password: 'root',
         database: 'db_reminder_kuliah',
@@ -37,28 +48,69 @@ fastify.get('/jadwal/stream', async function (request, reply) {
     }
 
     const [jadwal] = rows
+    console.log(jadwal.jam_mulai.substr(0, 5) == moment().format('HH:mm'))
     if (jadwal.jam_mulai.substr(0, 5) == moment().format('HH:mm')){
-        const [result = null] = googleTTS.getAllAudioUrls(`Mata kuliah ${jadwal.nm_mst_mata_kuliah}; akan segera di mulai di kelas ${jadwal.nm_mst_kelas}.`, {
-            lang: 'id',
-            slow: false,
-            host: 'https://translate.google.com',
-            splitPunct: ',.?',
-        }) 
+        const texts = [
+            `Mata kuliah ${jadwal.nm_mst_mata_kuliah}; akan segera di mulai di kelas ${jadwal.nm_mst_kelas}.`
+        ]
+        const listStream = []
+        let writestream = fs.createWriteStream('audio/finalaudio.mp3');
+        for (let i = 0; i<texts.length; i++){
+            const text = texts[i]
+            const getAudio = await axios.get('http://translate.google.com/translate_tts', {
+                params: {
+                    ie: 'UTF-8',
+                    client: 'tw-ob',
+                    tl: 'id',
+                    q: text
+                },
+                responseType: 'arraybuffer'
+            })
+        
+            await fs.writeFileSync(`audio/audio_${i}.mp3`, getAudio.data)
+
+            listStream.push(fs.createReadStream(`audio/audio_${i}.mp3`))
+        }
+
+        for (const stream of listStream){
+            stream.pipe(writestream)
+        }
+
         return {
             status: true,
-            url: result.url
+            url: `${baseUrl}/audio/finalaudio.mp3`
         }
     }
     if (jadwal.jam_selesai.substr(0, 5) == moment().format('HH:mm')){
-        const [result = null] = googleTTS.getAllAudioUrls(`Mata kuliah ${jadwal.nm_mst_mata_kuliah}; telah selesai di kelas ${jadwal.nm_mst_kelas}.`, {
-            lang: 'id',
-            slow: false,
-            host: 'https://translate.google.com',
-            splitPunct: ',.?',
-        }) 
+
+        const texts = [
+            `Mata kuliah ${jadwal.nm_mst_mata_kuliah}; telah selesai di kelas ${jadwal.nm_mst_kelas}.`
+        ]
+        const listStream = []
+        let writestream = fs.createWriteStream('finalaudio.mp3');
+        for (let i = 0; i<texts.length; i++){
+            const text = texts[i]
+            const getAudio = await axios.get('http://translate.google.com/translate_tts', {
+                params: {
+                    ie: 'UTF-8',
+                    client: 'tw-ob',
+                    tl: 'id',
+                    q: text
+                },
+                responseType: 'arraybuffer'
+            })
+        
+            await fs.writeFileSync(`audio/audio_${i}.mp3`, getAudio.data)
+
+            listStream.push(fs.createReadStream(`audio/audio_${i}.mp3`))
+        }
+
+        for (const stream of listStream){
+            stream.pipe(writestream)
+        }
         return {
             status: true,
-            url: result.url
+            url: `${baseUrl}/audio/finalaudio.mp3`
         }
     }
     
@@ -72,7 +124,7 @@ const startServer = async () => {
     try {
         const host = '0.0.0.0'
         const port = 9999
-        
+
 
         await fastify.listen(port, host)
         
